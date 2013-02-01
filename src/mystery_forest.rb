6f3b90f9729d9852f1122d3c8f744e2aa4a15dca
MarkHarder <mark.harder899@gmail.com>
2013-01-31 22:15:24 -0600

    Add utility module with clear screen method

--- src/mystery_forest.rb
+++ src/mystery_forest.rb
@@ -2,15 +2,21 @@
 # 2013.01.31
 # The game engine
 
+require_relative 'utility.rb'
+
+include Utility
+
 class MysteryForest
   def initialize
     @cmmnd = ""
   end
 
   def run
+    clear_screen
     while @cmmnd != "quit" do
       print "> "
       @cmmnd = gets.chomp
+      clear_screen
     end
   end
 end
---
+++ src/utility.rb
@@ -0,0 +1,9 @@
+# Mark Harder
+# 2013.01.31
+# A class of utility functions
+
+module Utility
+  def clear_screen
+    print "\e[2J\e[24H"
+  end
+end
